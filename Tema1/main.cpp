24f5e07c086554ab35f8ed687bc69cc99a415133
emicrisciu <crisciuemi@gmail.com>
2024-11-13 16:40:06 +0200

    Tema2: final version

--- Tema1/main.cpp
+++ Tema1/main.cpp
@@ -9,24 +9,27 @@ class Sportsman
         int age;
     
     public:
+        // constructor
         Sportsman(const std::string& firstName, const std::string& lastName, int age)
             : firstName(firstName), lastName(lastName), age(age)
             {
-                std::cout << "The Sportsman constructor has been invoked" << std::endl;
+                std::cout << "The Sportsman constructor has been invoked for " << this->getFullName() << std::endl;
             }
 
+        // copy constructor
         Sportsman(const Sportsman& s)
             : firstName(s.firstName), lastName(s.lastName), age(s.age)
             {
-                std::cout << "The Sportsman COPY constructor has been invoked" << std::endl;
+                std::cout << "The Sportsman COPY constructor has been invoked for " << this->getFullName() << std::endl;
             }
 
         // Sportsman(const Sportsman& s) = delete;
 
+        // move constructor
         Sportsman(Sportsman&& s)
             : firstName(std::move(s.firstName)), lastName(std::move(s.lastName)), age(std::move(s.age))
             {
-                std::cout << "The Sportsman MOVE constructor has been invoked" << std::endl;
+                std::cout << "The Sportsman MOVE constructor has been invoked for " << this->getFullName() << std::endl;
             }
 
         // Sportsman(Sportsman&& s) = delete;
@@ -77,24 +80,27 @@ class FootballPlayer : public Sportsman
         int goals;
 
     public:
+        // constructor
         FootballPlayer(const std::string& firstName, const std::string& lastName, int age, const std::string& position, const std::string& team)
             : Sportsman(firstName, lastName, age), position(position), team(team), goals(0)
             {
-                std::cout << "The FootballPlayer constructor has been invoked" << std::endl;
+                std::cout << "The FootballPlayer constructor has been invoked for " << this->getFullName() << std::endl;
             }
 
+        // copy constructor
         FootballPlayer(const FootballPlayer& f)
             : Sportsman(f), position(f.position), team(f.team), goals(f.goals)
             {
-                std::cout << "The FootballPlayer COPY constructor has been invoked" << std::endl;
+                std::cout << "The FootballPlayer COPY constructor has been invoked for " << this->getFullName() << std::endl;
             }
 
         // FootballPlayer(const FootballPlayer& f) = delete;
 
+        // move constructor
         FootballPlayer(FootballPlayer&& f)
             : Sportsman(std::move(f)), position(std::move(f.position)), team(std::move(f.team)), goals(std::move(f.goals))
             {
-                std::cout << "The FootballPlayer MOVE constructor has been invoked" << std::endl;
+                std::cout << "The FootballPlayer MOVE constructor has been invoked for " << this->getFullName() << std::endl;
             }
         
         // FootballPlayer(FootballPlayer&& f) = delete;
@@ -164,28 +170,31 @@ int main()
 {
     FootballPlayer *fp1 = new FootballPlayer("Lionel", "Messi", 37, "CF", "Inter Miami");
     FootballPlayer *fp2 = new FootballPlayer("Cristiano", "Ronaldo", 39, "ST", "Al Nassr");
+
     Sportsman *s = new FootballPlayer("Gheorghe", "Hagi", 40, "CAM", "Steaua Bucuresti");
+    Sportsman *s1 = new Sportsman(*s);
 
     FootballPlayer fp3("Adrian", "Mutu", 30, "ST", "Fiorentina");
     FootballPlayer fp4 = fp3;
 
+    FootballPlayer fp5("Lamine", "Yamal", 17, "RW", "FC Barcelona");
+
     fp1->displaySportsmanInfo();
     fp2->displaySportsmanInfo();
     s->displaySportsmanInfo();
+    s1->displaySportsmanInfo();
     fp3.displaySportsmanInfo();
     fp4.displaySportsmanInfo();
-
-    FootballPlayer fp5("Lamine", "Yamal", 17, "RW", "FC Barcelona");
     fp5.displaySportsmanInfo();
 
     FootballPlayer fp6 = std::move(fp5);
-    fp6.displaySportsmanInfo();
-    fp6.scoreGoal();
 
+    fp6.displaySportsmanInfo();
+    for(int i = 0; i < 3; i++) fp6.scoreGoal();
     fp5.displaySportsmanInfo();
     fp6.displaySportsmanInfo();
 
-    std::cout << "CONSTRUCTORS ZONE DONE!" << std::endl << std::endl;
+    std::cout << "\n" << "CONSTRUCTORS ZONE DONE!" << "\n" << std::endl;
     
     fp1->setFirstName("Lionel Andres");
     fp1->setAge(25);
@@ -197,13 +206,12 @@ int main()
     fp2->setPosition("LW");
 
     s->setAge(59);
-
-    fp1->displaySportsmanInfo();
-    fp2->displaySportsmanInfo();
-    s->displaySportsmanInfo();
+    s1->setFirstName("Ianis");
+    s1->setAge(25);
 
     for(int i = 0; i < 10; i++)
     {
+        if(i < 2) fp6.scoreGoal();
         if(i < 5) fp2->scoreGoal();
         fp1->scoreGoal();
     }
@@ -211,15 +219,21 @@ int main()
     fp1->displaySportsmanInfo();
     fp2->displaySportsmanInfo();
     s->displaySportsmanInfo();
+    s1->displaySportsmanInfo();
+    fp3.displaySportsmanInfo();
+    fp4.displaySportsmanInfo();
+    fp5.displaySportsmanInfo();
+    fp6.displaySportsmanInfo();
 
-    // *fp2 = *fp1;
+    *fp2 = *fp1;
 
-    // fp1->displaySportsmanInfo();
-    // fp2->displaySportsmanInfo();
+    fp1->displaySportsmanInfo();
+    fp2->displaySportsmanInfo();
 
     delete fp1;
     delete fp2;
     delete s;
+    delete s1;
 
     return 0;
 }
